15944da799e01129df76e3ba6622683d3cda0282
indramp3 <indraramadhanyuliadi@gmail.com>
2023-03-16 10:06:20 +0700

    Inisialisasi Framework

---
+++ src/main/java/com/basilisk/NewAppBasilisk/NewAppBasiliskApplication.java
@@ -0,0 +1,13 @@
+package com.basilisk.NewAppBasilisk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NewAppBasiliskApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NewAppBasiliskApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/basilisk/NewAppBasilisk/NewAppBasiliskApplicationTests.java
@@ -0,0 +1,13 @@
+package com.basilisk.NewAppBasilisk;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NewAppBasiliskApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
